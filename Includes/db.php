4a2b5f87b2460c402bf8bcafdf58a10aa60740f1
JWTappert <tappertj@oregonstate.edu>
2016-11-22 14:04:01 -0800

    Added Includes and db.php

---
+++ Includes/db.php
@@ -0,0 +1,8 @@
+<?php 
+	$dbServer = "localhost";
+	$dbUsername = "root";
+	$dbPassword = "Rubean1";
+	$dbName = "tempReadings";
+
+	$db = new mysqli($dbServer, $dbUsername, $dbPassword, $dbName);
+?>
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-	include_once(Includes/db.php);
+	include_once("/Includes/db.php");
 ?>
 
 <!DOCTYPE html>
