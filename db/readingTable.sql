62c28b59382a62d429aff526d75f7009a918f18e
Adam DuQuette <duquetteadam@gmail.com>
2016-11-22 14:43:12 -0800

    Added some test entries

--- db/readingTable.sql
+++ db/readingTable.sql
@@ -7,3 +7,7 @@ CREATE TABLE `readings` (
 );
 
 SET TIME_ZONE = '-08:00';
+
+INSERT INTO readings (node_id, temp, time) VALUES (0, 69.02, '2016-10-16 14:56:04');
+INSERT INTO readings (node_id, temp, time) VALUES (0, 70.02, '2016-11-16 14:56:04');
+INSERT INTO readings (node_id, temp, time) VALUES (1, 68.02, '2016-11-16 14:57:04');
