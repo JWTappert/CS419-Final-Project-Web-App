615ebdf522d0a44b3af4fd0a6ce449969a6f7b8d
Adam DuQuette <duquetteadam@gmail.com>
2016-11-22 14:39:37 -0800

    Modified sql file to update timezone info

--- db/readingTable.sql
+++ db/readingTable.sql
@@ -1,8 +1,9 @@
 CREATE TABLE `readings` (
-	`id` INT NOT NULL,
+	`id` INT NOT NULL auto_increment,
 	`node_id` INT(4) NOT NULL,
 	`temp` FLOAT(8) NOT NULL,
 	`time` TIMESTAMP NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
+SET TIME_ZONE = '-08:00';
