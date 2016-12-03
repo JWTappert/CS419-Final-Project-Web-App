0ca4e2d14638e06bb636d50e8bb5f956230e5126
JWTappert <tappertj@oregonstate.edu>
2016-12-03 15:10:47 -0800

    Removed third column from home page.

--- app.py
+++ app.py
@@ -16,14 +16,10 @@ def homepage():
 	cursor.execute("SELECT temp FROM readings WHERE node_id=%s ORDER BY time DESC LIMIT 1", (str(1))) 
 	node1 = cursor.fetchall()
 
-	cursor.execute("SELECT temp FROM readings WHERE node_id=%s ORDER BY time DESC LIMIT 1", (str(2))) 
-	node2 = cursor.fetchall()
-
 	temp0 = node0[0][0]
 	temp1 = node1[0][0]
-	temp2 = node2[0][0]
 
-	return render_template('index.html', temp0=temp0, temp1=temp1, temp2=temp2)
+	return render_template('index.html', temp0=temp0, temp1=temp1)
 
 
 if __name__ == "__main__":
