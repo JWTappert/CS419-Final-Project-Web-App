f92113712b4edd23a1df4e197d6894c85449bdfc
JWTappert <tappertj@oregonstate.edu>
2016-12-03 20:33:54 -0800

    Added the RP3 db info to the db_connect file

--- db_connect.py
+++ db_connect.py
@@ -1,7 +1,7 @@
 import MySQLdb
 
 def connection():
-	conn = MySQLdb.connect(host="localhost", user="root", passwd="password", db="heatMap")
+	conn = MySQLdb.connect(host="localhost", user="root", passwd="Rubean1", db="tempReadings")
 	cursor = conn.cursor()
 
 	return cursor, conn
