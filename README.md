d3a5b6a6d974f13c495738408eb25a7fe9ea3227
JWTappert <tappertj@oregonstate.edu>
2016-12-04 12:25:23 -0800

    Added dynamic Google chart. Currently updates on refresh

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # CS419-Final-Project-Web-App
 Contains the code for our web application that will be hosted on our Raspberry Pi.
+
+Need:
+Some kind of package manager (we use homebrew)
+Pip (maybe?)
+PySerial (sudo apt-get install python-serial)
+MySQLdb (pip install python-mysql)
+Flask
+
+
--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import datetime
 from flask import Flask, render_template
 from db_connect import connection
 
@@ -10,16 +11,36 @@ def homepage():
 	except Exception as e:
 		return(str(e))
 
-	cursor.execute("SELECT temp FROM readings WHERE node_id=%s ORDER BY time DESC LIMIT 1", (str(0))) 
+	#Build the google chart from data in the database
+	cursor.execute("SELECT time, temp FROM readings WHERE node_id=%s ORDER BY time DESC LIMIT 1440", (str(0)))
+	numrows = cursor.rowcount
 	node0 = cursor.fetchall()
 
-	cursor.execute("SELECT temp FROM readings WHERE node_id=%s ORDER BY time DESC LIMIT 1", (str(1))) 
+	tableData = [[] for _ in range(numrows+1)]
+	tableData[0] = ['Time', 'Living Room', 'Bedroom']
+
+	for n in xrange(0, numrows):
+		tempList = [node0[n][0], node0[n][1]]
+		tableData[n+1] = tempList
+
+	cursor.execute("SELECT temp FROM readings WHERE node_id=%s ORDER BY time DESC LIMIT 1440", (str(1)))
+	numrows = cursor.rowcount
 	node1 = cursor.fetchall()
 
-	temp0 = node0[0][0]
-	temp1 = node1[0][0]
+	for n in xrange(0, numrows):
+		tableData[n+1].append(node1[n][0])
 
-	return render_template('index.html', temp0=temp0, temp1=temp1)
+	#Get current temps
+	cursor.execute("SELECT temp, time FROM readings WHERE node_id=%s ORDER BY time DESC LIMIT 1", (str(0))) 
+	curr0 = cursor.fetchall()
+
+	cursor.execute("SELECT temp FROM readings WHERE node_id=%s ORDER BY time DESC LIMIT 1", (str(1))) 
+	curr1 = cursor.fetchall()
+
+	temp0 = curr0[0][0]
+	temp1 = curr1[0][0]
+
+	return render_template('index.html', temp0=temp0, temp1=temp1, data=tableData)
 
 
 if __name__ == "__main__":
