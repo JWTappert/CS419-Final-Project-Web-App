91db3838254c7b28e11320bc805c639a51a03506
JWTappert <tappertj@oregonstate.edu>
2016-12-05 11:02:56 -0800

    Attempt 2 at dynamic graph

--- app.py
+++ app.py
@@ -16,23 +16,36 @@ def homepage():
 	numrows = cursor.rowcount
 	node0 = cursor.fetchall()
 
-	tableData = [[] for _ in range(numrows+1)]
-	tableData[0] = ['Time', 'Living Room', 'Bedroom']
-
-	for n in xrange(0, numrows):
-		if node0[n][1] == 666 or node0[n][1] == 0:
-			continue
-		tempList = [node0[n][0], node0[n][1]]
-		tableData[n+1] = tempList
-
 	cursor.execute("SELECT temp FROM readings WHERE node_id=%s ORDER BY time ASC LIMIT 1440", (str(1)))
 	numrows = cursor.rowcount
 	node1 = cursor.fetchall()
 
+	tableData = [[] for _ in range(numrows+1)]
+	tableData[0] = ['Time', 'Living Room', 'Bedroom']
+
+	runTotal0 = 0
+	runTotal1 = 0
+
+	for n in xrange(0, numrows):
+		if node0[n][1] == 666 or node0[n][1] == 0:
+			node0[n][1] = float(runTotal0) / n+1
+			continue
+		elif node1[n][0] == 666 or node1[n][0] == 0:
+			node1[n][0] = float(runTotal1) / n+1
+			continue
+
+		tempList = [node0[n][0], node0[n][1], node1[n][0]]
+		tableData[n+1] = tempList
+		runTotal0 = node0[n][1]
+		runTotal1 = node1[n][0]
+
+	
+	'''
 	for n in xrange(0, numrows):
 		if node1[n][0] == 666 or node1[n][0] == 0:
 			continue
 		tableData[n+1].append(node1[n][0])
+	'''
 
 	#Get current temps
 	cursor.execute("SELECT temp, time FROM readings WHERE node_id=%s ORDER BY time DESC LIMIT 1", (str(0))) 
