7829be2cc4df4a37819c5164820c5f095ee279a6
JWTappert <tappertj@oregonstate.edu>
2016-12-05 23:32:33 -0800

    revised garbage collection code

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def homepage():
 		return(str(e))
 
 	#Build the google chart from data in the database
-	cursor.execute("SELECT time, temp FROM readings WHERE node_id=%s AND time > DATE_SUB(CURDATE(), INERVAL 1 DAY)", (str(0)))
+	cursor.execute("SELECT time, temp FROM readings WHERE node_id=%s AND time > DATE_SUB(CURDATE(), INTERVAL 1 DAY)", (str(0)))
 	#cursor.execute("SELECT time, temp FROM readings WHERE node_id=%s ORDER BY time ASC LIMIT 1440", (str(0)))
 	numrows = cursor.rowcount
 	node0 = cursor.fetchall()
@@ -29,10 +29,30 @@ def homepage():
 
 	for n in xrange(0, numrows):
 		if node0[n][1] == 666 or node0[n][1] == 0:
-			avgTemp = runTotal0 / (n + 1)
+			print "n: " + str(n)
+			runTotal0 = runTotal0 / (n+1)
+		else:
+			runTotal0 = 0 + node0[n][1]
+
+		if node1[n][0] == 666 or node1[n][0] == 0:
+			runTotal1 = runTotal1 / (n+1)
+		else:
+			runTotal1 = 0 + node1[n][0]
+
+		tempList = [node0[n][0], runTotal0, runTotal1]
+		tableData[n+1] = tempList
+
+	'''
+	for n in xrange(0, numrows):
+		if node0[n][1] == 666 or node0[n][1] == 0:
+			print "GOT GARBAGE in node0: " + str(node0[n][1])
+			avgTemp = 69.69 #runTotal0 / (n + 1)
+			print "avgTemp: " + str(avgTemp)
 			tempList = [node0[n][0], avgTemp, node1[n][0]]
 		elif node1[n][0] == 666 or node1[n][0] == 0:
-			avgTemp = runTotal1 / (n + 1)
+			print "GOT GARBAGE in node1: " + str(node1[n][0])
+			avgTemp = 69.69 #runTotal1 / (n + 1)
+			print "avgTemp: " + str(avgTemp)
 			tempList = [node0[n][0], node0[n][1], avgTemp]
 		else:
 			tempList = [node0[n][0], node0[n][1], node1[n][0]]
@@ -40,7 +60,7 @@ def homepage():
 		tableData[n+1] = tempList
 		runTotal0 = runTotal0 + node0[n][1]
 		runTotal1 = runTotal1 + node1[n][0]
-
+	'''
 	#Get current temps
 	cursor.execute("SELECT temp, time FROM readings WHERE node_id=%s ORDER BY time DESC LIMIT 1", (str(0))) 
 	curr0 = cursor.fetchall()
@@ -50,6 +70,8 @@ def homepage():
 
 	temp0 = curr0[0][0]
 	temp1 = curr1[0][0]
+
+	print tableData
 
 	conn.close()
 
